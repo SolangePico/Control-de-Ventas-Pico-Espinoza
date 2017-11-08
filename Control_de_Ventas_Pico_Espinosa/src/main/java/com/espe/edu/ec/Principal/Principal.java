5f68311ad3145e1b18cbcd7f3a9b065189dfe8d8
Solange Pico <svpico@espe.edu.ec>
2017-11-07 20:27:51 -0500

    Creacion de Clases necesarias para el programa y modelamiento de la interfaz del programa.

--- Control_de_Ventas_Pico_Espinosa/src/main/java/com/espe/edu/ec/Principal/Principal.java
+++ Control_de_Ventas_Pico_Espinosa/src/main/java/com/espe/edu/ec/Principal/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.espe.edu.ec.control_de_ventas_pico_espinosa;
+package com.espe.edu.ec.Principal;
 
 /**
  *
